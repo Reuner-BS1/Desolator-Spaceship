916592a8d3d2d1b075e64ed6730fd11b40298a99
Reuner-BS1 <c.reuner@schueler.bs1-bt.de>
2023-12-05 15:34:14 +0100

    createt src folder

---
+++ src/bullet.py
@@ -0,0 +1,10 @@
+class Bullet:
+    def __init__(self, x, y, sprite, speed, damage):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.speed = speed
+        self.damage = damage
+
+    def move(self, x, y):
+        pass
---
+++ src/spaceship.py
@@ -0,0 +1,13 @@
+class Spaceship:
+    def __init__(self, x, y, sprite):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.speed = 0.0
+
+    def move(self, x, y):
+        pass
+
+    def shoot(self):
+        bullet = "Bullet"
+        return bullet
