88ffa4cb2cbb07a8781d5a28723020a7cda2cb52
arthu <ar.cebaev@schueler.bs1-bt.de>
2023-12-05 15:43:15 +0100

    weapon class files Arthur

--- src/bigshotweapon.py
+++ src/bigshotweapon.py
@@ -1,6 +1,6 @@
 class Bigshotweapon:
     def __int__(self):
-        self.reload_time = "float"
+        self.reload_time = ""
 
     def shoot(self, x, y):
         pass
--- src/weapon.py
+++ src/weapon.py
@@ -1,6 +1,6 @@
 class Weapon:
     def __int__(self):
-        self.reload_time = "float"
+        self.reload_time = ""
 
     def shoot(self, x, y):
         pass
