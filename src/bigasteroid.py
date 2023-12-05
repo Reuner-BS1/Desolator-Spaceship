bc2a3c68357873b0c217c76185391468abb416a8
stefan.wirth <stef.wirth@schueler.bs1-bt.de>
2023-12-05 15:33:42 +0100

    Klassen "bigAsteroid" "smallasteroid" geschrieben

---
+++ src/bigasteroid.py
@@ -0,0 +1,14 @@
+def __init__(self, x, y, sprite, speed):
+    self.x = 0.0
+    self.y = 10.0
+    self.sprite = sprite
+    self.speed = 1.0
+    self.split_probability = ""
+    self.hp = 3
+
+
+def move(self, x, y):
+    pass
+
+def split(self):
+    pass
---
+++ src/smallasteroid.py
@@ -0,0 +1,10 @@
+def __init__(self, x, y, sprite, speed):
+    self.x = 0.0
+    self.y = 10.0
+    self.sprite = sprite
+    self.speed = 2.0
+    self.hp = 1
+
+
+def move(self, x, y):
+    pass
